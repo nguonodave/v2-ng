83b227bea2d4a74ee60981e8510d4937007dfaf4
David Ochiel <nguonodave@gmail.com>
2024-11-06 06:44:08 +0300

    refactor: change component name HeroFooter to Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Footer from "./Footer";
+
+export default function Home() {
+    return (
+        <div className="below-nav">
+            <section className="mid-content hero">
+                <div className="hero-text">
+                    <p className="hero-greet">Hey there, Dave here.</p>
+                    <h1 className="hero-name">David Ochiel</h1>
+                    <h1 className="hero-intro">
+                        I'm passionate about building digital solutions.
+                    </h1>
+                    <p className="hero-desc">
+                        Currently mastering Golang and JavaScript at{" "}
+                        <a href="https://www.zone01kisumu.ke/">
+                            <span>Zone01 Kisumu</span>
+                        </a>
+                        . I love creating clean, efficient, and dynamic software
+                        experiences.
+                    </p>
+                </div>
+                {/* <div className="me">
+                    <img src={me} alt="" />
+                </div> */}
+            </section>
+
+            <Footer />
+        </div>
+    );
+}
