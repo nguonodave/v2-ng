ab14da1076a2243c3b9bce6ea49ea939a288a70c
David Ochiel <nguonodave@gmail.com>
2024-11-07 17:43:16 +0300

    feat: add class name to main footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className="main-footer">
             <p>Designed and built by David Ochiel</p>
 
             <div className="socials">
