438624bc8bb4974c1a96125c524626fe54d656ad
David Ochiel <nguonodave@gmail.com>
2024-11-07 09:05:22 +0300

    refactor: separate projects functionality to its component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+
+export default function Projects() {
+    return (
+        <div className="below-nav">
+            <section className="mid-content">
+                <p>ProjectsPage</p>
+            </section>
+
+            <Footer />
+        </div>
+    );
+}
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,13 +1,9 @@
-import Footer from "../components/Footer";
+import Projects from "../components/Projects";
 
 export default function ProjectsPage() {
     return (
-        <div className="below-nav">
-            <section className="mid-content">
-                <p>ProjectsPage</p>
-            </section>
-
-            <Footer />
-        </div>
+        <>
+        <Projects />
+        </>
     );
 }
