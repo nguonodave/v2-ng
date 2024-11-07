cf5eab052f47905429eceadb2c5ba0fca01e5233
David Ochiel <nguonodave@gmail.com>
2024-11-07 06:32:41 +0300

    feat: add projects page

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import ProjectsPage from "./pages/ProjectsPage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
         </Route>
     )
 );
---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+
+export default function ProjectsPage() {
+    return (
+        <div className="below-nav">
+            <section className="mid-content">
+                <p>ProjectsPage</p>
+            </section>
+
+            <Footer />
+        </div>
+    );
+}
