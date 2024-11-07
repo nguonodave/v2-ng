149e8a598a68152d5f33aa0f9375744379659ebb
David Ochiel <nguonodave@gmail.com>
2024-11-07 04:58:16 +0300

    feat: implement a basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={ <h1>My App</h1> } />)
+)
+
 export default function App() {
     return (
-        <>
-            <Navbar />
-            <Home />
-        </>
+        // <>
+        //     <Navbar />
+        //     <Home />
+        // </>
+
+        <RouterProvider router={router} />
     );
 }
