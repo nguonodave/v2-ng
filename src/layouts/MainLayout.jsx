04dce288d470f327edce3735b135384bcc1fe771
David Ochiel <nguonodave@gmail.com>
2024-11-13 05:32:58 +0300

    feat: add vercel analytics

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MainLayout() {
     return (
         <>
             <Navbar />
+            <Analytics />
             <Outlet />
         </>
     );
