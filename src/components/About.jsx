2070e4b20b6bbc7611099f081aff5916081c3b9c
David Ochiel <nguonodave@gmail.com>
2025-12-15 18:51:41 +0300

    feat: update bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,88 +2,85 @@ import Footer from "../components/Footer";
 import { NavLink } from "react-router-dom";
 
 export default function About() {
-    return (
-        <div className="below-nav">
-            <section className="mid-content projects-page">
-                <div className="about-wrapper">
-                    <div className="about-text">
-                        <div className="paragraphs">
-                            <p>
-                                Hey there, Dave here. I'm a tech problem-solver
-                                and explorer, who thrives on building things
-                                that work—and work well. My journey into the
-                                world of tech started in my sophomore year at
-                                university, and it's been one wild ride ever
-                                since. I began with the basics of web
-                                development, and now I'm diving deep into
-                                full-stack applications that bring ideas to
-                                life.
-                            </p>
+  return (
+    <div className="below-nav">
+      <section className="mid-content projects-page">
+        <div className="about-wrapper">
+          <div className="about-text">
+            <div className="paragraphs">
+              <p>
+                David Ochiel is a tech problem-solver and creative thinker,
+                currently shaping the future of technology at Zone01 Kisumu. As
+                an aspiring Data Engineer, he thrives on transforming complex
+                problems into efficient, data-driven solutions that are both
+                reliable and innovative.
+              </p>
 
-                            <p>
-                                Currently, I'm{" "}
-                                <a
-                                    href="https://www.zone01kisumu.ke/"
-                                    target="_blank"
-                                    title="Recoding the world with African talent."
-                                >
-                                    <span>
-                                        shaping the future of tech at Zone01
-                                        Kisumu
-                                    </span>
-                                </a>
-                                , where I get to build intuitive,
-                                high-performance platforms using a mix of modern
-                                tools. Efficiency, clean code, and keeping up
-                                with the ever-evolving tech world are my driving
-                                forces—because let's face it, tech waits for no
-                                one.
-                            </p>
+              <p>
+                His journey into tech began in his sophomore year at university,
+                starting with web development and evolving into full-stack
+                applications and data engineering. Beyond coding, David's
+                creativity as a pencil artist has influenced his approach to
+                technology. He loves bringing ideas from imagination to life,
+                whether through sketches or building scalable software systems.
+              </p>
 
-                            <p>
-                                I'm always on the lookout for new challenges to
-                                tackle and cool projects to build. If you've got
-                                an idea or a problem to solve,{" "}
-                                <NavLink to="/contact">
-                                    <span>let's chat</span>
-                                </NavLink>{" "}
-                                and create something awesome.
-                            </p>
+              <p>
+                At{" "}
+                <a
+                  href="https://www.zone01kisumu.ke/"
+                  target="_blank"
+                  title="Recoding the world with African talent."
+                >
+                  <span>Zone01 Kisumu</span>
+                </a>
+                , he has built intuitive, high-performance platforms that
+                reflect his commitment to clean code, efficiency, and staying
+                ahead in an ever-evolving tech landscape.
+              </p>
 
-                            <p>What's in my toolbox?</p>
-                        </div>
+              <p>
+                Looking ahead, David aims to leverage his expertise in data
+                engineering and analytics to drive impactful solutions that
+                address global challenges, blending creativity, technology, and
+                ethical practices. His achievements in building functional,
+                user-centered platforms and his dedication to continuous
+                learning demonstrate both his skill set and his potential for
+                future leadership in tech.
+              </p>
 
-                        <div className="skills">
-                            <ul>
-                                <li>
-                                    <span>Backend:</span> Golang, Python
-                                    (Django)
-                                </li>
-                                <li>
-                                    <span>Frontend:</span> JavaScript (ES6),
-                                    React
-                                </li>
-                                <li>
-                                    <span>Database:</span> SQL
-                                </li>
-                                <li>
-                                    <span>Core Web:</span> HTML, CSS
-                                </li>
-                                <li>
-                                    <span>Development Tools & Platforms:</span>{" "}
-                                    Docker, Git, GitHub, Figma, Vercel, Netlify
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+              <p>What's in my toolbox?</p>
+            </div>
 
-                    <div className="about-image">
-                        <img src="../images/new-me-bg.jpg" alt="" />
-                    </div>
-                </div>
-            </section>
+            <div className="skills">
+              <ul>
+                <li>
+                  <span>Backend:</span> Golang, Python (Django)
+                </li>
+                <li>
+                  <span>Frontend:</span> JavaScript (ES6), React
+                </li>
+                <li>
+                  <span>Database:</span> SQL
+                </li>
+                <li>
+                  <span>Core Web:</span> HTML, CSS
+                </li>
+                <li>
+                  <span>Development Tools & Platforms:</span> Docker, Git,
+                  GitHub, Figma, Vercel, Netlify
+                </li>
+              </ul>
+            </div>
+          </div>
 
-            <Footer />
+          <div className="about-image">
+            <img src="../images/new-me-bg.jpg" alt="" />
+          </div>
         </div>
-    );
+      </section>
+
+      <Footer />
+    </div>
+  );
 }
